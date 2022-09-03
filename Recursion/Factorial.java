1aeded4c4697bd2a4f851a5dc9f97107b6ae1b77
Rishabh Jain <rishabhrj1508@gmail.com>
2022-09-04 02:07:56 +0530

    added code for Factorial and Fibonacci

---
+++ Recursion/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    static int fact(int n,int k){
+        if (n==0 || n==1) {
+            return k;
+        }
+        return fact(n-1, k*n);
+    }
+    public static void main(String[] args) {
+        System.out.println(fact(3, 1));
+    }
+}
